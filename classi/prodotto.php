2d29bf100a16c0e4ebd5d94cd31c0c74eaaa3310
EnricoCirilli <enrico.cirilli@gmail.com>
2024-01-10 12:47:56 +0000

    add: trait and prodotto non disponibile

--- classi/prodotto.php
+++ classi/prodotto.php
@@ -1,7 +1,45 @@
 <?php 
 
     include_once __DIR__ . '/category.php';
+    include_once __DIR__ . '/trait-iva.php';
+    class Prodotto{
+    use Iva;
+ public $nome;
+    public $prezzo;
 
+    public function __construct($nome, $prezzo)
+    {
+        $this->nome = $nome;
+        $this->prezzo = $prezzo;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getPrezzo()
+    {
+        return $this->prezzo;
+    }
+
+    public function setPrezzo($prezzo)
+    {
+        $this->prezzo = $prezzo;
+    }
+
+    public function calcolaIva()
+    {
+        return $this->prezzo * 0.22;
+    }
+}
+    // ...
+}
     class Prodotto{
         public $immagine;
         public $nome;
